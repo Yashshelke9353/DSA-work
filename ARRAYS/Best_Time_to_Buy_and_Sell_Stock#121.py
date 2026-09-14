1124599a068f39bbfa1428e33650c80b7a7ba9af
Yashshelke9353 <157669106+Yashshelke9353@users.noreply.github.com>
2026-09-14 15:02:11 +0530

    new problem

---
+++ ARRAYS/Best_Time_to_Buy_and_Sell_Stock#121.py
@@ -0,0 +1,16 @@
+def maxProfit(prices):
+    min_price = prices[0]
+    max_profit = 0
+    
+    for price in prices:
+        min_price = min(min_price, price)
+
+        profit = price - min_price
+
+        max_profit = max(max_profit, profit)
+
+    return max_profit
+
+prices =[7,1,5,3,6,4]
+result = maxProfit(prices)
+print(result)
