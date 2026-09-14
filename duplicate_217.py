e02c1230e304dfcebad7262a88bff1b4baef2219
Yashshelke9353 <157669106+Yashshelke9353@users.noreply.github.com>
2026-09-14 14:52:16 +0530

    Duplicate contain 217

---
+++ duplicate_217.py
@@ -0,0 +1,11 @@
+def containDuplicate(nums):
+    seen = set()
+    for num in nums:
+        if num in seen:
+            return True
+        seen.add(num)
+    return False
+
+nums = [1, 2, 3, 1]
+result = containDuplicate(nums)
+print(result)
