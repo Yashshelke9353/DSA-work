18a1f07969b4ef76420e9a438aa3ae37169ec043
Yashshelke9353 <157669106+Yashshelke9353@users.noreply.github.com>
2026-09-16 13:58:41 +0530

    new

--- ARRAYS/missing_number268.py
+++ ARRAYS/missing_number268.py
@@ -6,14 +6,22 @@
         # he varch answer pan chalel leetcode aprove karto
         
         
+# class Solution:
+#     def missingNumber(self, nums):
+#         n = len(nums)
+
+#         expected_sum = n * (n + 1) // 2
+#         actual_sum = sum(nums)
+
+#         return expected_sum - actual_sum
+
 class Solution:
     def missingNumber(self, nums):
         n = len(nums)
-
-        expected_sum = n * (n + 1) // 2
-        actual_sum = sum(nums)
-
-        return expected_sum - actual_sum
+        for i in range(0, n + 1):
+            if i not in nums:
+                return i
+            
     
 nums = [3, 0, 1]   # 
 solution = Solution()
