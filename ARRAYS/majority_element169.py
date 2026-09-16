fc2724f36ff6ed3f21782ecccd6b68b109b56b7e
Yashshelke9353 <157669106+Yashshelke9353@users.noreply.github.com>
2026-09-16 13:45:51 +0530

    mahority element

---
+++ ARRAYS/majority_element169.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        candidate=None
+        count=0
+        for num in nums:
+            if count==0:
+                candidate=num
+            if num == candidate:
+                count+=1
+            else:
+                count-=1
+        return candidate
+    
+nums = [2, 2, 1, 1, 1, 2, 2]
+solution=Solution()
+print(solution.majorityElement(nums))
+
+
+# class Solution(object):
+#     def majorityElement(self, nums):
+#         nums.sort()
+#         return nums[len(nums) // 2]
+
+#he answer pan chalte 
