183da8f41bdc8b001fd8b5e6278934a0590a13b5
Yashshelke9353 <157669106+Yashshelke9353@users.noreply.github.com>
2026-09-17 12:09:36 +0530

    product array

---
+++ ARRAYS/Product of Array Except Self 238.py
@@ -0,0 +1,25 @@
+# mhanje ek array maddhe swatala soddun baki sarvanchi multiplication 
+class Solution(object):
+    def productExceptSelf(self, nums):
+        n = len(nums)
+        
+        answer =[1]*n
+        
+        #prefix product
+        prefix=1
+        for i in range(n):
+            answer[i]=prefix
+            prefix*=nums[i]
+        
+        #suffix product
+        suffix=1
+        for i in range(n-1,-1,-1):
+            answer[i] *=suffix
+            suffix *=nums[i]
+            
+        return answer
+    
+nums = [1, 2, 3, 4]
+solution = Solution()
+print(solution.productExceptSelf(nums))  # Output: [24, 12, 8, 6]
+      
